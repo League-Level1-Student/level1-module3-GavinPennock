3cfdd98e38f5e0b15f146ad02adabb514dc6dc45
GavinPennock <50836070+GavinPennock@users.noreply.github.com>
2020-04-16 11:24:34 -0700

    only frogger and flappy birl left
    stay safe wear a :mask:

--- src/_07_cow_timer/CowTimer.java
+++ src/_07_cow_timer/CowTimer.java
@@ -7,14 +7,21 @@ package _07_cow_timer;
 import java.applet.AudioClip;
 import java.io.IOException;
 import javax.swing.JApplet;
+import javax.swing.JOptionPane;
 
 public class CowTimer {
 
 	/* 1. Make a constructor for the CowTimer class that initializes the minutes variable */
-	
+	private int minutes;
+
+	public CowTimer() {
+		
+		
+		minutes= 0;
+	}
 	/* 4. Complete the main method of the CowTimerRunner class */
 
-	private int minutes;
+	
 
 	public void setTime(int minutes) {
 		this.minutes = minutes;
@@ -26,16 +33,17 @@ public class CowTimer {
 		 * 2. Count down the minutes, print the current minute then sleep for the number of minutes
 		 * using Thread.sleep(int milliseconds). 
 		 */
-
+		Thread.sleep(minutes*60000);
 		/*
 		 * 3. When the timer is finished, use the playSound method to play a moo sound.
 		 * You can use the .wav file in the default package, or you can download one
 		 * from freesound.org, then drag it intothe default package.
 		 */
-
+		System.out.println("timer went off");
+		
 	}
 
-	private void playSound(String fileName) {
+	public void playSound(String fileName) {
 		AudioClip sound = JApplet.newAudioClip(getClass().getResource(fileName));
 		sound.play();
 	}
